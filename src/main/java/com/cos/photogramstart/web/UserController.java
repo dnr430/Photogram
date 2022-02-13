1df175c2ae730b143dca820d3f856e59709a5a7f
dnr430 <dnr430@gmail.com>
2022-02-13 22:38:40 +0900

    회원정보수정 시큐리티태그 라이브러리

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,9 +1,8 @@
 package com.cos.photogramstart.web;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -18,15 +17,18 @@ public class UserController {
 	}
 	
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails ) {
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
 		// 1. 추천!!
 		System.out.println("세션 정보 : " + principalDetails.getUser());
 		
 		// 2. 비추천..
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
-		System.out.println("직접 찾은 세션 정보1 : " + auth.getPrincipal());
-		System.out.println("직접 찾은 세션 정보2 : " + mPrincipalDetails.getUser());
+//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//		PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
+//		System.out.println("직접 찾은 세션 정보1 : " + auth.getPrincipal());
+//		System.out.println("직접 찾은 세션 정보2 : " + mPrincipalDetails.getUser());
+		
+//		model.addAttribute("principal", principalDetails.getUser());
+		
 		return "user/update";
 	}
 }
